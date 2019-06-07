7d78e27d53551391a6947f938117609850e1877f
Brandon Powell <brandon.powell@armory.io>
2019-06-07 14:38:00 -0700

    feat(plugins): start of plugin system

---
+++ orca-api/src/main/java/com/netflix/spinnaker/orca/pipeline/PluginStageDefintionBuilder.java
@@ -0,0 +1,6 @@
+package com.netflix.spinnaker.orca.pipeline;
+
+import com.netflix.spinnaker.orca.pipeline.StageDefinitionBuilder;
+
+public interface PluginStageDefintionBuilder extends StageDefinitionBuilder {
+}
